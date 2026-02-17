b099fe1dcefdb8e0e2f3aa1fb191a2e8419ea30f
Blankeos <carloantonioct@gmail.com>
2026-02-17 11:18:48 +0800

    chore: prerender the docs for deploy in pages.

--- apps/site/src/pages/+config.ts
+++ apps/site/src/pages/+config.ts
@@ -1,11 +1,12 @@
-import type { Config } from "vike/types"
-import vikePhoton from "vike-photon/config"
-import config from "vike-solid/config"
+import type { Config } from "vike/types";
+import vikePhoton from "vike-photon/config";
+import config from "vike-solid/config";
 
 // Default config (can be overridden by pages)
 export default {
-  extends: [config, vikePhoton],
-  photon: {
-    server: "src/server/server.ts",
-  },
-} satisfies Config
+	extends: [config, vikePhoton],
+	photon: {
+		server: "src/server/server.ts",
+	},
+	prerender: true,
+} satisfies Config;
